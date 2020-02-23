3a33dde901e89d4793352adc94f0f5606e96ebcd
tikken <tikken23@gmail.com>
2020-02-24 00:12:37 +0300

    fixtures

--- printers/src/DataFixtures/AppFixtures.php
+++ printers/src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\BlogPost;
@@ -10,25 +11,52 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-       $blogPost = new BlogPost();
+        $this->loadUsers($manager);
+        $this->loadBlogPosts($manager);
+    }
 
-       $blogPost->setTitle('A first post!');
-       $blogPost->setPublished(new \DateTime('2018-07-01 12:00:00'));
-       $blogPost->setContent('Post text!');
-       $blogPost->setAuthor('any');
-       $blogPost->setSlug('a-first-post');
+    public function loadBlogPosts($manager)
+    {
+        $user = $this->getReference('user_admin');
 
-       $manager->persist($blogPost);
+        $blogPost = new BlogPost();
 
-       $blogPost = new BlogPost();
+        $blogPost->setTitle('A first post!');
+        $blogPost->setPublished(new \DateTime('2018-07-01 12:00:00'));
+        $blogPost->setContent('Post text!');
+        $blogPost->setAuthor($user);
+        $blogPost->setSlug('a-first-post');
 
-       $blogPost->setTitle('A second post!');
-       $blogPost->setPublished(new \DateTime('2018-07-01 12:00:00'));
-       $blogPost->setContent('Second post text!');
-       $blogPost->setAuthor('any');
-       $blogPost->setSlug('a-second-post');
+        $manager->persist($blogPost);
 
-       $manager->persist($blogPost);
-       $manager->flush();
+        $blogPost = new BlogPost();
+
+        $blogPost->setTitle('A second post!');
+        $blogPost->setPublished(new \DateTime('2018-07-01 12:00:00'));
+        $blogPost->setContent('Second post text!');
+        $blogPost->setAuthor($user);
+        $blogPost->setSlug('a-second-post');
+
+        $manager->persist($blogPost);
+        $manager->flush();
+    }
+
+    public function loadComments($manager)
+    {
+
+    }
+
+    public function loadUsers($manager)
+    {
+        $user = new User();
+
+        $user->setUsername('tikken');
+        $user->setEmail('tikken23@gmail.com');
+        $user->setName('tikken');
+        $user->setPassword('secret12345');
+        $this->addReference('user_admin', $user);
+
+        $manager->persist($user);
+        $manager->flush();
     }
 }
