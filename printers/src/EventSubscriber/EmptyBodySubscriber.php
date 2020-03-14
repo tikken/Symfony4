eca9e9e97a377f6d6360cbbeb11b2efee3d46f41
tikken <tikken23@gmail.com>
2020-03-14 13:59:31 +0300

    empty body exception

---
+++ printers/src/EventSubscriber/EmptyBodySubscriber.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: tikken
+ * Date: 3/14/20
+ * Time: 1:11 PM
+ */
+
+namespace App\EventSubscriber;
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpFoundation\Request;
+use App\Exception\EmptyBodyException;
+
+class EmptyBodySubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::REQUEST => ['handleEmptyBody', EventPriorities::POST_DESERIALIZE]
+        ];
+    }
+
+    public function handleEmptyBody(RequestEvent $event)
+    {
+        $method = $event->getRequest()->getMethod();
+
+        if(!in_array($method, [Request::METHOD_POST, Request::METHOD_PUT]))
+        {
+            return;
+        }
+
+        $data = $event->getRequest()->get('data');
+
+        if(null === $data) {
+            throw new EmptyBodyException();
+        }
+    }
+}
---
+++ printers/src/Exception/EmptyBodyException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: tikken
+ * Date: 3/14/20
+ * Time: 1:26 PM
+ */
+
+namespace App\Exception;
+
+
+class EmptyBodyException extends \Exception
+{
+
+}
